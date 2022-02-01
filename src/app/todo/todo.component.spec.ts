5bbd6ef38714103fa1b36d50e152cac2e23224b7
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-02-01 15:43:44 +0100

    test(todo): customize todo service mock

--- src/app/todo/todo.component.spec.ts
+++ src/app/todo/todo.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MockProvider } from 'ng-mocks';
+import { MockProvider, ngMocks } from 'ng-mocks';
+import { EMPTY } from 'rxjs';
 
 import { TodoComponent } from './todo.component';
 import { TodoService } from './todo.service';
 
 describe(TodoComponent.name, () => {
+  beforeAll(() => {
+    ngMocks.autoSpy('jasmine');
+  });
+
+  afterAll(() => {
+    ngMocks.autoSpy('reset');
+  });
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [TodoComponent],
-      providers: [MockProvider(TodoService)],
+      providers: [
+        MockProvider(TodoService, {
+          list$: () => EMPTY,
+        }),
+      ],
     });
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
